f6aec62e26ee3ba26e8e6ba6cd27d0a8e681fec8
Ayan Singh <ayansingh2042@gmail.com>
2025-02-08 12:41:36 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import chefIcon from '../assets/chef-claude-icon.png'
+function Header(){
+    return(
+        <header>
+            <img src={chefIcon} alt="ayan" />
+            <h1>Chef AI</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+function Main(){
+    return(
+        <main>
+            <form className="add-form">
+                <input 
+                type="text"
+                placeholder="e.g. oregano"
+                aria-label="Add ingredient" />
+                <button>Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
+
+export default Main
