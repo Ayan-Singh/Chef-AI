e96f3ead1fbf2cde9e9736f64eaa33f09d046e02
Ayan Singh <ayansingh2042@gmail.com>
2025-02-10 23:46:01 +0530

    header done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react"
 function Main(){
+
+const [ingredients,setIngredients] =useState([]);
+
+const ingredientListItems=ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+))
+function handleSubmit(event){
+    event.preventDefault()
+    const formData =new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient");
+    setIngredients(prevIngredients => [...prevIngredients ,newIngredient])
+}
+
     return(
         <main>
-            <form className="add-form">
+            <form onSubmit={handleSubmit} className="add-form">
                 <input 
                 type="text"
                 placeholder="e.g. oregano"
-                aria-label="Add ingredient" />
+                aria-label="Add ingredient" 
+                name="ingredient"
+                />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
+            
         </main>
     )
 }
